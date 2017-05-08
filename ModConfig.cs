2bb5709d131946710bcd354060f97262bb756423
miketweaver <miketweaver@users.noreply.github.com>
2017-05-08 15:31:12 -0600

    rename default

--- ModConfig.cs
+++ ModConfig.cs
@@ -5,7 +5,7 @@ namespace DailyNews
     public class ModConfig
     {
         public bool showMessages { get; set; } = true;
-		public string defaultTexture { get; set; } = "assets/news.png";
+		public string defaultNewscaster { get; set; } = "assets/news.png";
         public string extension { get; set; } = "*.json";
         public string contentFolder { get; set; } = "news";
 	}
