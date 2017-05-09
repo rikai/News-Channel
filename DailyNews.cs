48471337e20549bcccb28d7231cffc07ca28a66a
Mike Weaver <miketweaver@users.noreply.github.com>
2017-05-08 23:12:18 -0600

    Add .png to NewsItems.json (#11)
    * move to news folder, re-factor config.json
    
    * move load to 'entry'
    
    * complete .png to json
    
    * cleanup

--- DailyNews.cs
+++ DailyNews.cs
@@ -9,7 +9,6 @@ using StardewValley;
 using StardewValley.Objects;
 using System.Collections.Generic;
 
-
 namespace DailyNews
 {
 
@@ -17,39 +16,44 @@ namespace DailyNews
     {
         private int dailyNews;
         private ModConfig config;
-        private Texture2D newsScreen;
-        private List<string> contentFolderFiles;
+        private ModData contentFiles;
+        private string[] contentFolderFiles;
         private string contentFolderExtension;
-        private List<string> combinedNewsItems = new List<string>();
+        private List<Headline> NewsItems = new List<Headline>();
         public string customContentFolder;
 
         public override void Entry(IModHelper helper)
         {
-            this.config = helper.ReadConfig<ModConfig>();
-
-            customContentFolder = Path.Combine(helper.DirectoryPath, this.config.contentFolder);
-            if (!Directory.Exists(customContentFolder))
-            {
-                Directory.CreateDirectory(customContentFolder);
-                showMessage("The '" + this.config.contentFolder + "' folder is empty. Mod broken.", 1);
-            }
-
-            this.newsScreen = helper.Content.Load<Texture2D>(@"assets\" + config.texture);
-            this.contentFolderExtension = config.extension;
-
-
-            SaveEvents.AfterLoad += (x, y) => Load();
-
+            Load();
             TimeEvents.DayOfMonthChanged += (x, y) => checkIfNews();
         }
 
         private void Load()
         {
+            //Read the config file.
+            this.config = this.Helper.ReadConfig<ModConfig>();
+            customContentFolder = Path.Combine(this.Helper.DirectoryPath, this.config.contentFolder);
+            if (!Directory.Exists(customContentFolder))
+            {
+                Monitor.Log("The '" + this.config.contentFolder + "' folder is empty. Mod broken.", LogLevel.Error);
+                Directory.CreateDirectory(customContentFolder);
+                this.Monitor.Log("Created the '" + this.config.contentFolder + "' folder", LogLevel.Warn);
+            }
+
+            this.contentFolderExtension = config.extension;
+
+            //Read the news files.
             contentFolderFiles = ParseDir(customContentFolder, contentFolderExtension);
             foreach (string file in contentFolderFiles)
             {
-                var contentFiles = this.Helper.ReadJsonFile<ModData>(file) ?? new ModData();
-                combinedNewsItems.AddRange(contentFiles.newsItems);
+                this.Monitor.Log("Loading: " + file, LogLevel.Trace);
+                contentFiles = this.Helper.ReadJsonFile<ModData>(file) ?? new ModData();
+                string newscasterFileName = string.IsNullOrWhiteSpace(contentFiles.newscaster)
+                    ? config.defaultNewscaster
+                    : contentFiles.newscaster;
+
+                foreach (string headlineItem in contentFiles.newsItems)
+                    NewsItems.Add(new Headline(headlineItem, newscasterFileName, file));
             }
         }
 
@@ -57,23 +61,36 @@ namespace DailyNews
         {
             CustomTVMod.removeChannel("News");
 
+            Random randomNews = new Random();
+            dailyNews = randomNews.Next(0, NewsItems.Count);
+
             string str = Game1.shortDayNameFromDayOfSeason(Game1.dayOfMonth);
-            string season = Game1.currentSeason;
+
             if (str.Equals("Tue") || str.Equals("Fri") || str.Equals("Sat"))
             {
+                string season = Game1.currentSeason;
                 CustomTVMod.addChannel("News", "News Report", deliverNews);
-                Random randomNews = new Random();
-                dailyNews = randomNews.Next(0, combinedNewsItems.Count);
 
                 if (config.showMessages)
                     showMessage("Breaking News for " + char.ToUpper(season[0]) + season.Substring(1) + " " + Game1.dayOfMonth, 2);
             }
         }
 
-        private void deliverNews(TV tv, TemporaryAnimatedSprite sprite, StardewValley.Farmer who, string answer)
+        private void deliverNews(TV tv, TemporaryAnimatedSprite sprite, Farmer who, string answer)
         {
+            Texture2D newsScreen = null;
+
+            try  //Try to load news screen from .json file
+            {
+                newsScreen = Helper.Content.Load<Texture2D>(NewsItems[dailyNews].Texture);
+            }
+            catch  //If the load failed, just load the default newscaster.
+            {
+                Monitor.Log("Unable to load newscaster(" + NewsItems[dailyNews].Texture + ") defined in: " + NewsItems[dailyNews].Source, LogLevel.Error);
+            }
+
             TemporaryAnimatedSprite newsSprite = new TemporaryAnimatedSprite(newsScreen, new Rectangle(0, 0, 42, 28), 150f, 2, 999999, tv.getScreenPosition(), false, false, (float)((double)(tv.boundingBox.Bottom - 1) / 10000.0 + 9.99999974737875E-06), 0.0f, Color.White, tv.getScreenSizeModifier(), 0.0f, 0.0f, 0.0f, false);
-            string text = combinedNewsItems[dailyNews];
+            string text = NewsItems[dailyNews].HeadlineText;
             CustomTVMod.showProgram(newsSprite, text, CustomTVMod.endProgram);
         }
 
@@ -84,27 +101,10 @@ namespace DailyNews
             Game1.addHUDMessage(hudmsg);
         }
 
-        private List<string> ParseDir(string path, string extension)
+        private string[] ParseDir(string path, string extension)
         {
-            List<string> customFiles = new List<string>();
-            foreach (string dir in Directory.EnumerateDirectories(path))
-            {
-                ParseDir(Path.Combine(path, dir), extension);
-            }
-
-            foreach (string file in Directory.EnumerateFiles(path))
-            {
-                if (Path.GetExtension(file) == extension)
-                {
-                    string filePath = Path.Combine(path, Path.GetDirectoryName(file), Path.GetFileName(file));
-                    customFiles.Add(filePath);
-                    Monitor.Log(filePath);
-                }
-            }
-            return customFiles;
+            return Directory.GetFiles(path, extension, SearchOption.AllDirectories);
         }
-
-
     }
 
 }
